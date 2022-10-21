647ddb004dab9217eb4556e3aedc9792e18767fb
Daniel Lavoie <dlavoie@live.ca>
2022-10-21 05:22:06 -0400

    fix(ci): permissions on ssh private key

--- .concourse/tasks/publish/publish.sh
+++ .concourse/tasks/publish/publish.sh
@@ -190,6 +190,7 @@ export M2_HOME=~/.m2 && \
   mkdir ~/.ssh && \
   ssh-keyscan github.com >> ~/.ssh/known_hosts && \
   echo $GIT_PRIVATE_KEY > ~/.ssh/id_rsa && \
+  chmod 600 ~/.ssh/id_rsa && \
   git config --global user.email "${GIT_USER_EMAIL}" && \ 
   git config --global user.name "${GIT_USER_NAME}" && \
   git add --all && \
